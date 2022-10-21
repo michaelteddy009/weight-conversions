a795fb4fd9bbbe5ca5c4ca1e9dee27cbbafa9d64
michaelteddy009 <michaelteddy009@users.noreply.github.com>
2022-10-21 05:33:22 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,25 +4,25 @@ namespace Lido\WeightConversions;
 
 class Weight
 {
-	protected float $kilograms;
+    protected float $kilograms;
 
-	public static function kilograms(float $kilograms)
-	{
-		return new static($kilograms);
-	}
+    public static function kilograms(float $kilograms)
+    {
+        return new static($kilograms);
+    }
 
-	public function __construct(float $kilograms)
-	{
-		return $this->kilograms = $kilograms;
-	}
+    public function __construct(float $kilograms)
+    {
+        return $this->kilograms = $kilograms;
+    }
 
-	public function toPounds()
-	{
-		return $this->kilograms * 2.2046;
-	}
+    public function toPounds()
+    {
+        return $this->kilograms * 2.2046;
+    }
 
-	public function toStones()
-	{
-		return $this->kilograms * 0.15747;
-	}
+    public function toStones()
+    {
+        return $this->kilograms * 0.15747;
+    }
 }
