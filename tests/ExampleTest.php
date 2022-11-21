23ee922081c3e8bb909ffaf1aa5a82e2931709fe
michaelteddy009 <michaelteddy009@gmail.com>
2022-11-21 08:06:39 +0300

    testing update

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,5 +9,5 @@ it('can convert kilograms to punds correctly', function () {
 
 it('can convert kilograms to stones correctly', function () {
     $pounds = Weight::kilograms(100)->toStones();
-    expect($pounds)->toEqual(15.747);
+    expect($pounds)->toEqual(15.7477);
 });
